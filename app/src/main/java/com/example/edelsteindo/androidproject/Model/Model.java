74b0608cdb52380d80c1b1d85587ed54296811be
Dor Edelstein <Doredel98@gmail.com>
2017-07-16 01:31:20 +0300

    chaged model class name to Model

---
+++ app/src/main/java/com/example/edelsteindo/androidproject/Model/Model.java
@@ -0,0 +1,14 @@
+package com.example.edelsteindo.androidproject.Model;
+
+/**
+ * Created by Dor-New on 15/07/2017.
+ */
+
+public class Model {
+    public final static Model instace = new Model();
+
+    private MemModel modelMem;
+    private SqlModel modelSql;
+
+
+}
